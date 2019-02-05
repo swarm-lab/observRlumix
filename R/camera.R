085c88096fa3d6832cba23f75b7f026bda54a800
Simon Garnier <garnier@njit.edu>
2019-02-05 15:13:52 -0500

    Initial commit.

---
+++ R/camera.R
@@ -0,0 +1,25 @@
+#' @export
+initCams <- function(cameras) {
+  lapply(cameras, function(x) {
+    RCurl::getURL(paste0("http://", x, "/cam.cgi?mode=camcmd&value=recmode"))
+  })
+
+  NULL
+}
+
+
+#' @export
+grabPictures <- function(cameras) {
+  lapply(cameras, function(x) {
+    RCurl::getURL(paste0("http://", x, "/cam.cgi?mode=camcmd&value=capture"))
+  })
+
+  NULL
+}
+
+
+
+
+
+
+
---
+++ R/observR.R
@@ -0,0 +1,33 @@
+#' @export
+observR <- function(wemo, cameras) {
+  magicLamp::wemo_ON(wemo)
+  initCams(cameras)
+  magicLamp::wemo_OFF(wemo)
+
+  ui <- svDialogs::dlgInput(message = "Duration of the experiment in seconds")
+  duration <- as.numeric(gsub("[^0-9]", "", ui$res))
+
+  ui <- svDialogs::dlgInput(message = "Interval between 2 pictures in seconds")
+  interval <- as.numeric(gsub("[^0-9]", "", ui$res))
+
+  times <- now() + seq(0, duration, interval) * 1000
+
+  pb <- progress::progress_bar$new(total = length(times), format = "Progress [:bar] :percent")
+
+  for (i in seq_len(length(times))) {
+    magicLamp::wemo_ON(wemo)
+    Sys.sleep(2)
+
+    grabPictures(cameras)
+
+    pb$tick()
+
+    magicLamp::wemo_OFF(wemo)
+
+    if (i < length(times)) {
+      Sys.sleep((times[i + 1] - now()) / 1000)
+    }
+  }
+
+  NULL
+}
